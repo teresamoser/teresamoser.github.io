23cb33cc2237373095452b451e065bc1773ea07e
Teresa <moserte@byui.edu>
2018-03-19 00:46:07 -0600

    fix html

--- assignments/weathersite/scripts/wu-weather.js
+++ assignments/weathersite/scripts/wu-weather.js
@@ -12,7 +12,7 @@ weatherObject.onload = function () {
     document.getElementById('place').innerHTML = weatherInfo.current_observation.display_location.full;
     document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
     document.getElementById('wspeed').innerHTML = weatherInfo.current_observation.wind_mph;
-    document.getElementById('w_icon').src = weatherInfo.current_observation.icon_url;
+    document.getElementById('wicon').src = weatherInfo.current_observation.icon_url;
     document.getElementById('cw').innerHTML = weatherInfo.current_observation.weather;
     document.getElementById('currentF').innerHTML = weatherInfo.current_observation.forecast_url;
 } 
